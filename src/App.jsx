a4a0a1b3e7fd2d8fca2bcb9fb9082a853bd46dbb
Daniela Ventura <daniela.mv343@gmail.com>
2024-07-02 13:16:13 -0600

    Add styles for cards and delete button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,35 @@ import Card from "./Components/Card"
 import GlobalStyles from "./Components/GlobalStyles"
 import styled from "styled-components"
 
+const AppContainer = styled.div`
+  width:1280px;
+  max-width:100%;
+  margin: 0 auto;
+  min-height:100vh;
+`
+const Header = styled.header`
+   padding: 60px 0;
+   display: flex;
+   justify-content: space-between;
+`
+
+const CardContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+`
+const ProductsContainer = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    background: none;
+    padding: 20px;
+    width: 100%;
+    text-align: center;
+    box-sizing: border-box;
+    justify-content: space-around;
+`
+
 const App = () => {
 
   const [products, setProducts] = useState([])
@@ -20,31 +49,33 @@ const App = () => {
 
   return (
     <>
-    <GlobalStyles />
-      <main>
-  
-        <section>
-          <div>
-            <ul>
-              {products.map(({ id, title, price, description, category, image}) => {
-                return (
-                  <Card
-                    key={id}
-                    id={id} 
-                    title={title}
-                    price={price}
-                    description={description}
-                    image={image}
-                    category={category}
-                    products={products} 
-                    setProducts={setProducts}               
-                  />
-                )
-              })}
-            </ul>
-          </div>
-        </section>
-      </main>
+        <GlobalStyles />
+        <AppContainer>
+          <Header />
+        <main>
+          <section>
+            <CardContainer>
+              <ProductsContainer>
+                {products.map(({ id, title, price, description, category, image }) => {
+                  return (
+                    <Card
+                      key={id}
+                      id={id}
+                      title={title}
+                      price={price}
+                      description={description}
+                      image={image}
+                      category={category}
+                      products={products}
+                      setProducts={setProducts}
+                    />
+                  )
+                })}
+              </ProductsContainer>
+            </CardContainer>
+          </section>
+        </main>
+        </AppContainer>
     </>
   )
 }
--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,7 +1,73 @@
 import React from "react"
+import styled from "styled-components"
 
 
-const Card = ({id, title, price, description, category, image, products, setProducts}) => {
+const ProductCard = styled.li`
+  background: #f2f2f2;
+  padding: 20px;
+  width: 100%; 
+  flex: 0 0 300px; 
+  list-style: none; 
+  border-radius: 9px;
+  box-shadow: 0 6px 18px rgba(0, 0, 0, 0.5);
+`
+
+const Title = styled.h1`
+    font-size: 28px;
+    font-weight: 700;
+    margin-bottom: 10px;;
+`
+const Image = styled.img`
+  width: 100%;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  background: none;
+  box-shadow: 0 0px 5px rgba(0, 0, 0, 0.1);
+`
+
+const Price = styled.p`
+  font-size: 20px;
+  margin: 10px 0;
+  font-weight: 700;
+`
+
+const Description = styled.p`
+  font-size: 18px;
+  color: #201e1e;
+  margin-bottom: 10px;
+  text-align: justify;
+`
+
+const Category = styled.p`
+  font-weight: bold;
+  font-size: 18px;
+  font-style: italic;
+  text-align: start;
+  color: #8a4914;
+  span{
+        width: 212px;
+        color: #3b3838;
+        font-weight: 400;
+        font-style: normal;
+    }
+`
+
+const DeleteButton = styled.button`
+  background-color: #7959cf;
+  margin-top: 20px;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: #6f3bc3f8;
+  }
+`
+
+
+const Card = ({ id, title, price, description, category, image, products, setProducts }) => {
 
     const handleDeleteProduct = (id) => {
         console.log("Eliminar producto", id)
@@ -11,14 +77,14 @@ const Card = ({id, title, price, description, category, image, products, setProd
 
 
     return (
-        <li key={id}>
-            <h1>{title}</h1>
-            <img style={{ width: "5rem" }} src={image} alt={title} />
-            <p><strong>Price: </strong>{price}</p>
-            <p>{description}</p>
-            <p><strong>Category: </strong>{category}</p>
-            <button onClick={() => handleDeleteProduct(id)}>Eliminar</button>
-        </li>
+            <ProductCard key={id}>
+                <Title>{title}</Title>
+                <Image src={image} alt={title} />
+                <Price>Price: {price}</Price>
+                <Description>{description}</Description>
+                <Category>Category: <span>{category}</span></Category>
+                <DeleteButton onClick={() => handleDeleteProduct(id)}>Eliminar</DeleteButton>
+            </ProductCard>
     )
 }
 
